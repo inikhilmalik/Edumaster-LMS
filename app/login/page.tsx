a1644894fdbac1e3c7af762bcf1d2564cd21e8d1
nikhil malik <nikhil@kogo.ai>
2025-12-12 09:50:10 +0530

    add mergin in card footer

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -76,7 +76,7 @@ export default function LoginPage() {
               />
             </div>
           </CardContent>
-          <CardFooter className="flex flex-col gap-4">
+          <CardFooter className="flex flex-col gap-4 mt-2">
             <Button type="submit" className="w-full" disabled={loading}>
               {loading ? (
                 <>
--- app/register/page.tsx
+++ app/register/page.tsx
@@ -134,7 +134,7 @@ export default function RegisterPage() {
               />
             </div>
           </CardContent>
-          <CardFooter className="flex flex-col gap-4">
+          <CardFooter className="flex flex-col gap-4 mt-2">
             <Button type="submit" className="w-full" disabled={loading}>
               {loading ? (
                 <>
